3552f0e3b8bf59461b90d9b3c4a8e8dd0164bb3c
m6ydz642 <m6ydz642@gmail.com>
2020-10-25 01:44:26 +0900

    get방식 post방식

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 메인 애플리케이션, 엔트리 애플리케이션
  var express = require('express');
 var app = express(); 
+var bodyParser = require('body-parser') // post방식 사용할 수 있게 해주는 모듈
 app.locals.pretty = true; // 글씨 이쁘게
 
 app.set('view engine','jade'); // 약속된 의미임
@@ -8,8 +9,9 @@ app.set('views', './views'); // jade 익스프레스에서는 views라는 디렉
 // app.set 뷰가 없어도 기본값으로 알아서 해줌 (views만)
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false })) // 모듈을 붙임
 
-app.get('/topic', function(req,res){
+app.get('/topic/:id', function(req,res){
 
       var topices = [
             'JavaScript is....',
@@ -18,16 +20,46 @@ app.get('/topic', function(req,res){
       ];
 
       var output = `
-      <a href="/topic?id=0">자바스크립트</a> <br>
-      <a href="/topic?id=1">노드 js </a> <br>
-      <a href="/topic?id=2">익스프레스</a> <br>
-      ${topices[req.query.id]}
+      <a href="/topic/0">자바스크립트</a> <br>
+      <a href="/topic/1">노드 js </a> <br>
+      <a href="/topic/2">익스프레스</a> <br>
+      ${topices[req.params.id]}
       `
+      // params -> 파라메터 줄인말
       res.send(output);
       console.log(output + "output내용");
       // res.send(topices[req.query.id]);
 
 }) 
+
+app.get('/form', function(req,res){
+      res.render('form');
+})
+
+app.get('/testview', function(req,res){
+      res.send('test html');
+})
+
+app.get('/form_receiver', function(req,res){
+      var title = req.query.title;
+      var decription = req.query.dec;
+      res.send(title + ',' + decription);
+       console.log(title + " : title 내용 ");
+      res.send("get?");
+});
+
+app.post('/form_receiver',function(req,res){
+      var title = req.body.title;
+      var desc = req.body.dec;
+      res.send(title + ',' + desc);
+      
+ res.send('post 방식');
+})
+app.get('/topic/:id/:mode', function(req, res){
+      res.send(req.params.id + ',' + req.params.mode)
+      // mode -> 마지막 /에 의미하는 정보
+})
+
 
 app.get('/template', function(req,res) {
       res.render('temp', {time:Date(), _title:'JadeTitle'}); // 웹페이지로 랜더링 해서 전송함
